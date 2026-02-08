35f5750e5adf5e73560b931205a2b529166abe3f
marko.djordjic988 <marko.djordjic988@gmail.com>
2026-02-08 03:26:00 +0100

    ignore

--- key_pairs.tf
+++ key_pairs.tf
@@ -1,19 +1,19 @@
 resource "aws_key_pair" "jenkins_key" {
   key_name   = "jenkins_key"
-  public_key = file("~/.ssh/jenkins_key.pub")
+  public_key = file("/keys/jenkins_key.pub")
 }
 
 resource "aws_key_pair" "sonar_key" {
   key_name   = "sonar_key"
-  public_key = file("~/.ssh/sonar_key.pub")
+  public_key = file("/keys/sonar_key.pub")
 }
 
 resource "aws_key_pair" "nexus_key" {
   key_name   = "nexus_key"
-  public_key = file("~/.ssh/nexus_key.pub")
+  public_key = file("/keys/nexus_key.pub")
 }
 
 resource "aws_key_pair" "ansible_key" {
   key_name   = "ansible_key"
-  public_key = file("~/.ssh/ansible_key.pub")
+  public_key = file("/keys/ansible_key.pub")
 }
